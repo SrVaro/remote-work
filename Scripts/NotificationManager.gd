46368284810cf1925e750ea447aeaaebf081e854
Varo <sr.varo07@gmail.com>
2023-10-15 18:44:17 +0200

    improved UI, added Time of day and stats

--- Scripts/NotificationManager.gd
+++ Scripts/NotificationManager.gd
@@ -1,8 +1,11 @@
 extends Node2D
 class_name NotificationManager
 
-signal notification(notification)
-signal remove_notification()
+signal add_notification(notification)
+signal remove_first_notification()
+signal remove_last_notification()
+
+signal notification_timeout()
 
 var notificationList = []
 
@@ -13,24 +16,28 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	var time = Time.get_ticks_usec()
-	if (time % 450) == 0:
-		_add_notification_to_queue()
-
 func _add_notification_to_queue():
 	var notification_instance = notification_scene.instantiate()
+	notification_instance.connect("notification_timeout_expired", _notification_timeout_expired)
 	notificationList.append(notification_instance)
-	notification.emit(notification_instance)
-	pass
+	UI._add_notification(notification_instance)
 	
 func _remove_last_notification() -> void:
 	if notificationList.size() != 0:
 		notificationList.remove_at(notificationList.size()-1)
-		remove_notification.emit()
+		UI._remove_last_notification()
+		
+func _remove_first_notification() -> void:
+	if notificationList.size() != 0:
+		notificationList.remove_at(0)
+		UI._remove_first_notification()
 		
 func _is_notification_list_empty():
 	return notificationList.size() == 0
+	
+func _notification_timeout_expired() -> void:
+	_remove_first_notification()
+	notification_timeout.emit()
+	
 	
 
--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -1,14 +1,39 @@
 extends CanvasLayer
-class_name UI
 
-@onready var v_box_container = $Control/MarginContainer/VBoxContainer
+@onready var time_of_day = $Control/MarginContainer/VBoxContainer2/TimeOfDay
+@onready var job_status = $Control/MarginContainer/VBoxContainer3/HBoxContainer2/JobStatus
+@onready var health = $Control/MarginContainer/VBoxContainer3/HBoxContainer/Health
+@onready var rest = $Control/MarginContainer/VBoxContainer3/HBoxContainer/Rest
+@onready var notification_list = $Control/MarginContainer/NotificationList
+
+
 
 func _add_notification(notification) -> void:
 	if(notification):
-		v_box_container.add_child(notification)
+		notification_list.add_child(notification)
 
 func _remove_last_notification() -> void:
-	var notifications = v_box_container.get_children(false)
+	var notifications = notification_list.get_children(false)
 	if notifications.size() != 0:
 		notifications[notifications.size() - 1].queue_free()
+	
+func _remove_first_notification() -> void:
+	var notifications = notification_list.get_children(false)
+	if notifications.size() != 0:
+		notifications[0].queue_free()
+		
+func _set_time(minutes: int, seconds: int):
+	var day_format_string:String = "%02d:%02d" % [minutes, seconds]
+
+	time_of_day.text = day_format_string
+	
+func _set_health(health_value: int):
+	health.value = health_value
+	
+func _set_job_status(job_status_value: int):
+	job_status.value = job_status_value
+	
+func _set_rest(rest_value: int):
+	rest.value = rest_value
+	
 
